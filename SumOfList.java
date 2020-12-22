6601e14b9857d172b4f654aca291664859b62fbc
Aditya Raute <43912470+dumbcoder7@users.noreply.github.com>
2020-12-23 01:09:14 +0530

    Fixed a typo

--- SumOfList.java
+++ SumOfList.java
@@ -3,7 +3,7 @@ public class SumOfList {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n, sum = 0;
-        System.out.println("Enter sequence if digits : ");
+        System.out.println("Enter sequence of digits : ");
         while(true){
             try{
                 sum +=sc.nextInt();
